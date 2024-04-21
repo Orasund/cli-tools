e0a5a24ecacb7ef4b8c1ff562ce4b4cfa7ba91db
Fritz Payr <fritz.payr@caplas.at>
2024-04-21 17:53:30 +0200

    Refactoring.

--- diffFiles/src/diff_files.py
+++ diffFiles/src/diff_files.py
@@ -4,86 +4,106 @@ import click
 
 
 @dataclass
+class Position:
+    x: int
+    y: int
+
+
+@dataclass
 class Node:
-    next_pos: tuple[int, int]
+    next_pos: Position
     length: int
 
 
-def build_subsequence_matrix(l1: list[str], l2: list[str]) -> list[list[Node]]:
+@dataclass
+class Matrix:
+    nodes: list[list[Node]]
+
+    def get_node(self, pos: Position) -> Node | None:
+        if pos.x < 0 or pos.y < 0:
+            return None
+        return self.nodes[pos.x][pos.y]
+
+    @classmethod
+    def make(cls, l1: list[str], l2: list[str]) -> "Matrix":
+        def compute_node(i1: int, i2: int, line1, line2) -> Node:
+            next_pos: Position | None
+            length: int
+
+            if line1 == line2:
+                next_pos = Position(i1 - 1, i2 - 1)
+                length = result.nodes[next_pos.x][next_pos.y].length + 1
+            else:
+                candidate1 = Position(i1, i2 - 1)
+                candidate2 = Position(i1 - 1, i2)
+                node1 = result.get_node(candidate1)
+                node2 = result.get_node(candidate2)
+
+                # find the candidate with the bigger length
+                if node1 is None and node2 is None:
+                    next_pos = None
+                    length = 0
+                elif node2 is not None and (node1 is None or node1.length < node2.length):
+                    next_pos = candidate2
+                    length = node2.length
+                else:
+                    next_pos = candidate1
+                    length = cast(Node, node1).length
+
+            return Node(next_pos, length)
+
+        result = Matrix([])
+        for i1, line1 in enumerate(l1):
+            result.nodes.append([])
+            for i2, line2 in enumerate(l2):
+                result.nodes[i1].append(compute_node(i1, i2, line1, line2))
+        return result
+
+
+def build_subsequence_matrix(l1: list[str], l2: list[str]) -> Matrix:
     # we use the matrix as a graph
     # where each node points to one of their neighbors
-    nodes: list[list[Node]] = []
-
-    def get_node(pos: tuple[int, int]) -> Node | None:
-        return nodes[pos[0]][pos[1]] if pos[0] >= 0 and pos[1] >= 0 else None
-
-    def compute_node(i1: int, i2: int) -> Node:
-        next_pos: tuple[int, int] | None
-        length: int
-
-        if l1[i1] == l2[i2]:
-            next_pos = (i1 - 1, i2 - 1)
-            length = nodes[next_pos[0]][next_pos[1]].length + 1
-        else:
-            candidate1 = (i1, i2 - 1)
-            candidate2 = (i1 - 1, i2)
-            node1 = get_node(candidate1)
-            node2 = get_node(candidate2)
-
-            # find the candidate with the bigger length
-            if node1 is None and node2 is None:
-                next_pos = None
-                length = 0
-            elif node2 is not None and (node1 is None or node1.length < node2.length):
-                next_pos = candidate2
-                length = node2.length
-            else:
-                next_pos = candidate1
-                length = cast(Node, node1).length
-
-        return Node(next_pos, length)
-
-    for i1 in range(len(l1)):
-        nodes.append([])
-        for i2 in range(len(l2)):
-            nodes[i1].append(compute_node(i1, i2))
+    nodes = Matrix.make(l1, l2)
     return nodes
 
 
-def compute_common_indices(nodes: list[list[Node]]) -> list[tuple[int, int]]:
-    commonIndices: list[tuple[int, int]] = []
-    if not nodes:
-        return commonIndices
+def compute_common_indices(nodes: Matrix) -> list[Position]:
+    common_indices: list[Position] = []
+    if not nodes.nodes:
+        return common_indices
 
-    pos: tuple[int, int] = (len(nodes) - 1, len(nodes[0]) - 1)
+    pos = Position(len(nodes.nodes) - 1, len(nodes.nodes[0]) - 1)
 
-    def get_node(pos: tuple[int, int]) -> Node:
-        return nodes[pos[0]][pos[1]]
+    def get_node(p: Position) -> Node:
+        result = nodes.get_node(p)
+        if result is None:
+            return Node(Position(0, 0), 0)
+        return result
 
     node: Node = get_node(pos)
 
     while node.next_pos is not None:
-        nextNode = get_node(node.next_pos)
+        next_node = get_node(node.next_pos)
 
-        if nextNode.length < node.length:
-            commonIndices.append(pos)
+        if next_node.length < node.length:
+            common_indices.append(pos)
 
         pos = node.next_pos
-        node = nextNode
+        node = next_node
 
-    commonIndices.reverse()
-    return commonIndices
+    common_indices.reverse()
+    return common_indices
 
 
 def compute_differences(
-    lines1: list[str], lines2: list[str], commonsIndices: list[tuple[int, int]]
+        lines1: list[str], lines2: list[str], commons_indices: list[Position]
 ):
-    indices: list[tuple[int, int]] = (
-        [(-1, -1)] + commonsIndices + [(len(lines1), len(lines2))]
+    indices: list[Position] = (
+            [Position(-1, -1)] + commons_indices + [Position(len(lines1), len(lines2))]
     )
 
     return [
-        (lines1[iMin[0] + 1 : iMax[0]], lines2[iMin[1] + 1 : iMax[1]])
+        (lines1[iMin.x + 1: iMax.x], lines2[iMin.y + 1: iMax.y])
         for (iMin, iMax) in zip(indices, indices[1:])
     ]
 
@@ -102,9 +122,9 @@ def diff_files(file1: IO, file2: IO):
     # 1. Build a graph of subsequences (stores as a matrix)
     matrix = build_subsequence_matrix(lines1, lines2)
     # 2. Walk the graph and collect all the indecies where the strings match
-    commonIndices = compute_common_indices(matrix)
+    common_indices = compute_common_indices(matrix)
     # 3. Compute the differences based on the collected indecies
-    differences = compute_differences(lines1, lines2, commonIndices)
+    differences = compute_differences(lines1, lines2, common_indices)
 
     click.echo(differences)
 
--- diffFiles/tests/test_diff_files.py
+++ diffFiles/tests/test_diff_files.py
@@ -1,7 +1,7 @@
 import os
 
 from click.testing import CliRunner
-from diff_files import diff_files
+from diff_files import diff_files, build_subsequence_matrix, compute_common_indices, compute_differences
 
 
 def test_empty():
@@ -23,3 +23,19 @@ def test_basic():
                 "(['\\n'], ['!'])"
                 "]\n")
     assert result.output == expected
+
+
+def test_diff():
+    os.chdir(os.path.dirname(__file__))
+    with open("data/file1") as f1, open("data/file2") as f2:
+        lines1 = f1.readlines()
+        lines2 = f2.readlines()
+    matrix = build_subsequence_matrix(lines1, lines2)
+    # 2. Walk the graph and collect all the indecies where the strings match
+    common_indices = compute_common_indices(matrix)
+    # 3. Compute the differences based on the collected indecies
+    differences = compute_differences(lines1, lines2, common_indices)
+    assert differences == [
+        (['Hello \n'], []),
+        (['beautiful\n'], ['little\n', 'boy\n', 'lives\n', 'in\n', 'his\n', 'own\n']),
+        (['\n'], ['!'])]
