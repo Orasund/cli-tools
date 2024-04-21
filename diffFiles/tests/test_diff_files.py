cd240c99845cb34f9970eae2c2562014c6fa70e6
Fritz Payr <fritz.payr@caplas.at>
2024-04-21 16:48:30 +0200

    Allow running tests from anywhere.

--- diffFiles/tests/test_diff_files.py
+++ diffFiles/tests/test_diff_files.py
@@ -1,17 +1,21 @@
+import os
+
 from click.testing import CliRunner
-from src.diff_files import diff_files
+from diff_files import diff_files
 
 
 def test_empty():
+    os.chdir(os.path.dirname(__file__))
     runner = CliRunner()
-    result = runner.invoke(diff_files, ["tests/data/empty", "tests/data/empty"])
+    result = runner.invoke(diff_files, ["data/empty", "data/empty"])
     assert result.exit_code == 0
     assert result.output == "[([], [])]\n"
 
 
 def test_basic():
+    os.chdir(os.path.dirname(__file__))
     runner = CliRunner()
-    result = runner.invoke(diff_files, ["tests/data/file1", "tests/data/file2"])
+    result = runner.invoke(diff_files, ["data/file1", "data/file2"])
     expected = ("["
                 "(['Hello \\n'], []), "
                 "(['beautiful\\n'], "
