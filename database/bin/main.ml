4e7290a23fa8978c7989b2bc6c57075b56c03c4a
Lucas Payr <orasund@gmail.com>
2024-05-06 18:45:21 +0200

    finish db

--- database/bin/main.ml
+++ database/bin/main.ml
@@ -1,8 +1,22 @@
 open Cmdliner
-open Database
+
+let read =
+  Term.(
+    const Database.Operations.read 
+    $ Arg.(value (pos 0 string "" (Arg.info ["key"])) )
+  )
+     
+let write = 
+  Term.(
+    const Database.Operations.write 
+    $ Arg.(value (pos 0 string "" (Arg.info ["key"])) )
+    $ Arg.(value (pos 0 string "" (Arg.info ["value"])) )
+  )
 
 let cmd = Cmd.group (Cmd.info "database")
-  [ Cmd.v (Cmd.info "init") (Term.const init) ]
+  [ Cmd.v (Cmd.info "init") (Term.const Database.Operations.init);
+  Cmd.v (Cmd.info "read") read;
+  Cmd.v (Cmd.info "write") write ]
 
 
 
--- database/lib/operations.ml
+++ database/lib/operations.ml
@@ -1,5 +1,58 @@
+
+module StringCompare = struct
+  type t = string
+  let compare = String.compare
+end
+
+module StringMap = Map.Make(StringCompare)
 let init = 
-    let
-      full_permissions = 0o777
-    in
-    Sys.mkdir "db" full_permissions
+  let
+    full_permissions = 0o777
+  in
+  Sys.mkdir "db" full_permissions
+
+let parse string =
+    match String.split_on_char '\t' string with
+    | k :: v:: _ -> Some (k,v)
+    | _ -> None
+
+let decode string =
+    string
+    |> Str.global_replace (Str.regexp_string "\\t") "\t"
+    |> Str.global_replace (Str.regexp_string "\\n") "\n"
+
+let encode string =
+  string
+  |> Str.global_replace (Str.regexp_string "\t") "\\t"
+  |> Str.global_replace (Str.regexp_string "\n") "\\n"
+
+let getDB =
+  let channel = open_in "db" in
+  let output = 
+    channel
+      |> In_channel.input_lines
+      |> List.filter_map parse
+      |> StringMap.of_list
+  in
+  close_in channel;
+  output
+
+let write key value =
+  let channel = open_out "db" in
+  StringMap.fold
+    (fun k v _ -> 
+      Printf.fprintf channel "%s\n"
+      ([k; v]|> String.concat "\t" )
+    )
+    (StringMap.add key value getDB)
+    ();
+  Out_channel.flush channel;
+  close_out channel ;;
+
+let read key =
+  getDB
+  |> StringMap.find key
+  |> print_endline
+  
+  
+
